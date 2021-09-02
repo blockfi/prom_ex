8f74484af2e9e54d74d785f537dc9e6e56b371dc
rybex <tomasz.rybczynski@blockfi.com>
2021-09-02 13:19:54 +0200

    Support mulltiple apps

--- lib/prom_ex/plug.ex
+++ lib/prom_ex/plug.ex
@@ -2,8 +2,16 @@ defmodule PromEx.Plug do
   @moduledoc """
   Use this plug in your Endpoint file to expose your metrics. The following options are supported by this plug:
 
-  * `:prom_ex_module` - The PromEx module whose metrics will be published through this particular plug
+  * `:prom_ex_module` - The PromEx module/modules whose metrics will be published through this particular plug
   * `:path` - The path through which your metrics can be accessed (default is "/metrics")
+
+  ```elixir
+  plug PromEx.Plug, prom_ex_module: YourApp.PromEx
+  ```
+
+  ```elixir
+  plug PromEx.Plug, prom_ex_module: [YourFirstApp.PromEx, YourSecondApp.PromEx]
+  ```
 
   If you need to have some sort of access control around your metrics endpoint, I would suggest looking at another
   library that I maintain called `Unplug` (https://hex.pm/packages/unplug). Using `Unplug`, you can skip over this plug
@@ -55,27 +63,52 @@ defmodule PromEx.Plug do
   end
 
   @impl true
+  def call(%Conn{request_path: metrics_path} = conn, %{metrics_path: metrics_path, prom_ex_module: prom_ex_module})
+      when is_list(prom_ex_module) do
+    metrics =
+      Enum.reduce(prom_ex_module, "", fn module, acc ->
+        case get_metrics(module) do
+          {:ok, metrics} -> acc <> metrics
+          {:error, _} -> acc <> ""
+        end
+      end)
+
+    case metrics do
+      "" -> respond(conn, 503, "Service Unavailable")
+      metrics -> respond(conn, 200, metrics)
+    end
+  end
+
+  @impl true
   def call(%Conn{request_path: metrics_path} = conn, %{metrics_path: metrics_path, prom_ex_module: prom_ex_module}) do
-    case PromEx.get_metrics(prom_ex_module) do
-      :prom_ex_down ->
-        Logger.warn("Attempted to fetch metrics from #{prom_ex_module}, but the module has not been initialized")
-
-        conn
-        |> put_resp_content_type("text/plain")
-        |> send_resp(503, "Service Unavailable")
-        |> halt()
-
-      metrics ->
-        PromEx.ETSCronFlusher.defer_ets_flush(prom_ex_module.__ets_cron_flusher_name__())
-
-        conn
-        |> put_resp_content_type("text/plain")
-        |> send_resp(200, metrics)
-        |> halt()
+    case get_metrics(prom_ex_module) do
+      {:ok, metrics} -> respond(conn, 200, metrics)
+      {:error, error} -> respond(conn, 503, error)
     end
   end
 
   def call(conn, _opts) do
     conn
+  end
+
+  defp get_metrics(prom_ex_module) do
+    case PromEx.get_metrics(prom_ex_module) do
+      :prom_ex_down ->
+        Logger.warn("Attempted to fetch metrics from #{prom_ex_module}, but the module has not been initialized")
+
+        {:error, "Service Unavailable"}
+
+      metrics ->
+        PromEx.ETSCronFlusher.defer_ets_flush(prom_ex_module.__ets_cron_flusher_name__())
+
+        {:ok, metrics}
+    end
+  end
+
+  defp respond(conn, status, response) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(status, response)
+    |> halt()
   end
 end
